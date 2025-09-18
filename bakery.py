a2eefdd25716205b3c44decd72fe2bbc715dd66a
Annie G <manyiannieg@Annies-MacBook-Air.local>
2025-09-17 22:47:48 -0700

    finished

--- bakery.py
+++ bakery.py
@@ -1,23 +1,44 @@
-# Test Case:
-# Input:
-# Enter the number of bread types: 3
-# Enter the number of days: 4
-# Enter sales for bread type 1: 5 7 8 6
-# Enter sales for bread type 2: 3 4 2 5
-# Enter sales for bread type 3: 9 6 7 8 
-
-# Note that the sales for bread type input comes in as a list of strings. 
-
-
-# Expected Output:
-# Total sales for each bread: [26, 14, 30]
-# Average daily sales: [5.66, 5.66, 5.66, 6.33]
-
-# If you feel overwhelmed by the problem, try writing down 1. the information you need, 2. a representation og the 2D array, and
-# 3. the steps you need to take to solve the problem. 
-
 def bakery():
+    # Enter
+    num_bread = int(input("Enter the number of bread types: "))
+    num_days = int(input("Enter the number of days: "))
+    
+    # create a list 
+    # # make sure input can add into the data 
+    # # for: in num_bread range # append data(list) // data should be in a list
+    sales = []
+    for i in range(num_bread):
+        # https://www.w3schools.com/python/ref_string_split.asp [split] 
+        # https://www.geeksforgeeks.org/python/python-map-function/ [map]
+        data = list(map(int, input(f"Enter sales for bread type {i+1}: ").split()))
+        sales.append(data)
+
+    # total sales for each bread type
+    # create list; go through; add the sum in the list i created 
+    bread_totals = []
+    for row in sales:
+        bread_totals.append(sum(row))
 
 
+    # first try
+    # reason wrong: calculating the rows and cols
+    # solution: use another for to get each day's cols
+    #day_avgs = []
+    #for col in sales:
+      #  avg = sum (col) / num_bread
+      #  day_avgs.append(avg)
 
-  
+    day_avgs = []
+    for col in range (num_days):
+        # remember to initialize the total
+        total = 0
+        for row in range (num_bread):
+            total += sales [row][col] # taking the cols
+        avg = total / num_bread
+        day_avgs.append (avg)
+
+
+    print("Total sales for each bread:", bread_totals)
+    print("Average daily sales:", day_avgs)
+
+bakery()
